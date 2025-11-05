bfb21112418d6568443f67c09f37cb522e482e04
Denise Mignoli <denisemignoli@gmail.com>
2025-11-04 23:48:01 -0300

    feat: Create abstract Animal base class for domain model

---
+++ src/main/java/com/farmhand/domain/Animal.java
@@ -0,0 +1,48 @@
+package com.farmhand.domain;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.UUID;
+
+public abstract class Animal {
+    private final String id;
+    private final String name;
+    private final LocalDate birthDate;
+    private double weight;
+
+    public Animal(String name, LocalDate birthDate, double weight){
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.birthDate = birthDate;
+        this.weight = weight;
+    }
+
+    public abstract String makeSound();
+
+    public int getAge() {
+        return Period.between(this.birthDate, LocalDate.now()).getYears();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        if (weight < 0) {
+            throw new IllegalArgumentException("O peso deve ser positivo!");
+        }
+        this.weight = weight;
+    }
+}
